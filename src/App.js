ef2b44a2bd9505d1c1bbab98d6146d3e55b80777
Gilbert <steven.n.gilbert@gmail.com>
2017-06-30 22:37:59 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import React, { Component } from 'react';
 import './App.css';
-const marked = require('marked');
+import marked from 'marked';
 
-class App extends Component {
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true
+});
+
+function Markdown(props) {
+  return (
+    <textarea id="markdown" onInput={props.onInput}>
+    </textarea>
+  );
+}
+
+function Preview (props) {
+  return (
+    <div id="preview">
+      {props.showHTML}
+    </div>
+  );
+}
+
+class Type extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      html: '',
+    };
+    this.handleMarkdown = this.handleMarkdown.bind(this);
+  }
+
+    handleMarkdown(e) {
+      const text = e.target.value;
+      let html = this.state.html.slice;
+
+      marked(text, function (err, content) {
+        if (err) {
+          throw err;
+        } 
+        html = content;
+      });
+      this.setState({
+          html: html,
+        });
+    }
+
+  render() {
+    return (
+      <main>
+        <Markdown onInput={this.handleMarkdown}/>
+        <Preview showHTML={this.state.html}/>
+      </main>
+    );
+  }
+}
+
+class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header>
           <h1>Gilbert's Markdown Previewer</h1>
         </header>
-        
-        <main>
-          <textarea id="markdown" ></textarea>
-          <div id="preview"></div>
-        </main>
-
+        <Type />
       </div>
     );
   }
 }
 
-console.log(marked('I am using __markdown__.'));
-
 export default App;
+
