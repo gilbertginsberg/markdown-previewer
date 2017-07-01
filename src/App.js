44dc6ffce08fe5537e86e7c1e435037a74aabb10
Gilbert <steven.n.gilbert@gmail.com>
2017-07-01 19:45:36 -0400

    v1

--- src/App.js
+++ src/App.js
@@ -14,10 +14,13 @@ function Markdown(props) {
   );
 }
 
-function Preview (props) {
+function Preview(props) {
+  
   return (
-    <div id="preview">
-      {props.showHTML}
+    <div id="preview-container">
+      <div id="preview">
+
+      </div>
     </div>
   );
 }
@@ -31,6 +34,23 @@ class Type extends React.Component {
     this.handleMarkdown = this.handleMarkdown.bind(this);
   }
 
+    renderPreview(markdown) {
+      const previewContainer = document.getElementById('preview-container');
+      const preview = document.getElementById('preview');
+      const firstChild = previewContainer.firstChild;
+      const html = document.createRange().createContextualFragment(markdown);
+
+      if (preview) {
+        const div = document.createElement('div');
+        div.appendChild(html);
+        previewContainer.replaceChild(div, preview);
+      } else {
+        const div = document.createElement('div');
+        div.appendChild(html);
+        previewContainer.replaceChild(div, firstChild);
+      }
+    }
+
     handleMarkdown(e) {
       const text = e.target.value;
       let html = this.state.html.slice;
@@ -43,7 +63,7 @@ class Type extends React.Component {
       });
       this.setState({
           html: html,
-        });
+        }, () => this.renderPreview(this.state.html)); 
     }
 
   render() {
@@ -70,4 +90,3 @@ class App extends React.Component {
 }
 
 export default App;
-
